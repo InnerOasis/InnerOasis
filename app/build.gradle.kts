0bef22f42950f3dfcd596a1a12db135bc14e906a
tvill08 <63677685+TereV32@users.noreply.github.com>
2023-11-21 18:36:47 -0600

    Implemented Youtube API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
     }
 
     buildTypes {
@@ -41,6 +42,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.pierfrancescosoffritti.androidyoutubeplayer:core:12.1.0")
+    implementation("com.codepath.libraries:asynchttpclient:2.2.0")
+    implementation("com.google.code.gson:gson:2.8.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/inneroasis/MainActivity.kt
+++ app/src/main/java/com/example/inneroasis/MainActivity.kt
@@ -1,48 +1,54 @@
 package com.example.inneroasis
 
-import SampleAdapter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView1: RecyclerView = findViewById(R.id.sensoryRecyclerView)
-        recyclerView1.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-
-        val dataList1 = generateSampleData()
-        val adapter1 = SampleAdapter(dataList1)
-        recyclerView1.adapter = adapter1
-
-        val recyclerView2: RecyclerView = findViewById(R.id.natureRecyclerView)
-        recyclerView2.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-
-        val dataList2 = generateSampleData()
-        val adapter2 = SampleAdapter(dataList2)
-        recyclerView2.adapter = adapter2
-
-        val recyclerView: RecyclerView = findViewById(R.id.relaxingRecyclerView)
-        recyclerView.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-
-        val dataList = generateSampleData()
-        val adapter = SampleAdapter(dataList)
-        recyclerView.adapter = adapter
-
-    }
-    // comment teresa branch
-    private fun generateSampleData(): List<SampleData> {
-        val dataList = mutableListOf<SampleData>()
-        for (i in 1..10) {
-            dataList.add(SampleData("Item $i"))
-        }
-        return dataList
+        val supportFragmentManager = supportFragmentManager
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.content, YoutubeVideoFragment(), null).commit()
     }
 }
+
+
+//        val recyclerView1: RecyclerView = findViewById(R.id.sensoryRecyclerView)
+//        recyclerView1.layoutManager =
+//            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+//
+////        val adapter1 = YoutubeRecyclerViewAdapter(youtubeVideos = , context = )
+////        recyclerView1.adapter = adapter1
+//
+//        val recyclerView2: RecyclerView = findViewById(R.id.natureRecyclerView)
+//        recyclerView2.layoutManager =
+//            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+//
+//        val dataList2 = generateSampleData()
+//        val adapter2 = SampleAdapter(dataList2)
+//        recyclerView2.adapter = adapter2
+//
+//        val recyclerView: RecyclerView = findViewById(R.id.relaxingRecyclerView)
+//        recyclerView.layoutManager =
+//            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+//
+//        val dataList = generateSampleData()
+//        val adapter = SampleAdapter(dataList)
+//        recyclerView.adapter = adapter
+//
+//    }
+//
+//    // comment teresa branch
+//    private fun generateSampleData(): List<SampleData> {
+//        val dataList = mutableListOf<SampleData>()
+//        for (i in 1..10) {
+//            dataList.add(SampleData("Item $i"))
+//        }
+//        return dataList
+//    }
+//}
