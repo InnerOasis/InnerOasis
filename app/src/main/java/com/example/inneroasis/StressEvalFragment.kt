3f255b6decc3323283dc8a2c0b6bea1d4b4e8129
Benjamin Cardenas <benjaminc5751@gmail.com>
2023-12-02 18:55:00 -0600

    Added a Stress Rating Dialog and a current stress level. Cleaned up code.

--- app/src/main/java/com/example/inneroasis/StressEvalFragment.kt
+++ app/src/main/java/com/example/inneroasis/StressEvalFragment.kt
@@ -9,10 +9,12 @@ import android.widget.Button
 import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import android.widget.TextView
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.app.AlertDialog
+
 
 class StressEvalFragment : Fragment() {
+    private val logTag: String by lazy { getString(R.string.log_tag) }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,13 +24,27 @@ class StressEvalFragment : Fragment() {
         val stressButton = view.findViewById<Button>(R.id.stress_Button)
         val seekBar = view.findViewById<SeekBar>(R.id.stressSeekBar)
         val stressRating = view.findViewById<TextView>(R.id.stress_rating)
+        val currentStressRating = view.findViewById<TextView>(R.id.currentStressLevel)
 
+        //function for showing stress level dialog
+        fun showStressRatingDialog() {
+            val currentProgress = seekBar.progress
+            val message = "Current Stress Level: $currentProgress"
+
+            AlertDialog.Builder(requireContext())
+                .setTitle("Stress Rating")
+                .setMessage(message)
+                .setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .show()
+        }
+
+        // Stress seekBar
         seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-
                 stressRating.text = "Stress Level: $progress"
-
-
+                updateStressLevelDescription(progress)
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -38,21 +54,35 @@ class StressEvalFragment : Fragment() {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
                 // Called when the user stops interacting with the SeekBar
             }
-
-
         })
 
-        stressButton.setOnClickListener{
-            Log.v("Stress button was clicked", stressButton.toString())
-
+        stressButton.setOnClickListener {
+            Log.d(logTag, "Button was clicked. Stress Level: ${seekBar.progress}")
+            currentStressRating.text = "Current Stress Level: ${seekBar.progress}"
+            showStressRatingDialog()
         }
-
-
-
-
 
         return view
     }
 
+    private fun updateStressLevelDescription(progress: Int) {
+        when (progress) {
+            in 1..3 -> {
+                // Handle the case for low stress level
+            }
+            in 4..6 -> {
+                // Handle the case for moderate stress level
+            }
+            in 7..10 -> {
+                // Handle the case for high stress level
+            }
+            else -> {
+                // Handle unexpected values
+            }
+        }
 
+    }
 }
+
+
+
